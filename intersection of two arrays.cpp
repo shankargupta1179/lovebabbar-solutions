8be6766e0052b88a772a991e862f128f2ff365c3
Shankar gupta <shankarguptha9676@gmail.com>
2021-05-31 19:02:40 +0530

    Add files via upload

---
+++ intersection of two arrays.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#include<set>
+using namespace std;
+void getIntersection(int a[], int n, int b[], int m)
+{
+	
+	set<int> s;
+	
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<m;j++)
+		{
+			if(a[i]==b[j])
+			{
+				s.insert(a[i]);
+			}
+		}
+	}
+	
+	for (auto itr = s.begin(); itr != s.end(); itr++)
+        cout << *itr
+             << " "; 
+}
+
+// Driver Code
+int main()
+{
+	int a[9] = { 1, 2, 5, 6, 2, 3, 5, 7, 3 };
+	int b[10] = { 2, 4, 5, 6, 8, 9, 4, 6, 5, 4 };
+
+	getIntersection(a, 9, b, 10);
+}
+
+// contributed by Anirban Chand
+
